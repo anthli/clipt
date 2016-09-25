149e54110f4309e050dd76c713f791b3eaffd67b
Anthony Li <anthli1895@gmail.com>
2016-09-25 14:43:17 -0400

    Added search bar at the bottom of the window

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,12 +6,12 @@ angular.module('clipt.controllers', [])
 
 .controller('MainCtrl', ['$scope', ($scope) => {
   $scope.clips;
-  $scope.clipIndex;
+  $scope.index;
   $scope.search;
 
   // Get the index of the clip that was clicked on
   $scope.getClip = (index) => {
-    $scope.clipIndex = index;
+    $scope.index = index;
   };
 
   // Render the clips that were received from the main process
@@ -24,8 +24,8 @@ angular.module('clipt.controllers', [])
   // When a clip is clicked on, send it back to the main process
   $(document).dblclick((event) => {
     // Ignore double-clicks that weren't inside the area of a clip
-    if ($scope.clipIndex !== undefined) {
-      ipcRenderer.send('clip-copied', $scope.clips[$scope.clipIndex]);
+    if ($scope.index !== undefined) {
+      ipcRenderer.send('clip-copied', $scope.clips[$scope.index]);
     }
   });
 }]);
